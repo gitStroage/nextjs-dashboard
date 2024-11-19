2e02906c4779d6d4ea2913d65224ad0bd5ae547a
Randolph <zhangkai905106@gmail.com>
2024-11-19 18:06:07 +0800

    "End of tutorial"

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,19 @@
-export default function Page(){
-    return (<p className="p-6 bg-green-50">Customer Page</p>)
+import Table from "@/app/ui/customers/table"
+import { fetchCustomers, fetchFilteredCustomers } from "@/app/lib/data"
+
+export default async function Page(props: {
+    searchParams?: Promise<{
+        query?: string,
+        page?: string
+    }>
+}) {
+    const searchParams = await props.searchParams
+    const query = searchParams?.query || ""
+    const customers = await fetchFilteredCustomers(query)
+    console.log()
+    return (<>
+        <div>
+            <Table customers={customers} />
+        </div>
+    </>)
 }
